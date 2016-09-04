15c5466954cd3cf3e5b5f6e9a80bae2cbd009fec
wolfram74 <phaugen@gmail.com>
2016-09-03 21:04:32 -0500

    I am not above cheap ploys

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -4,4 +4,7 @@ $(document).ready( function(){
   console.log('jammin!')
   var landingHtml = Handlebars.compile($('#landingPage').html())
   $('body').html(landingHtml)
+  setTimeout(function(){
+    $('h1').html('SURPRISE HUGS!')
+  }, 1500)
 })
