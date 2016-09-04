e4fccc252d97186df49aeb44b9e07c1673b302e1
wolfram74 <phaugen@gmail.com>
2016-09-03 20:59:01 -0500

    hey mister blue sky

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('something happened.')
+/*
+*/
+$(document).ready( function(){
+  console.log('jammin!')
+  var landingHtml = Handlebars.compile($('#landingPage').html())
+  $('body').html(landingHtml)
+})
